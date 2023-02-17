0f9f6a44a2cc48bec82b5cdec41cd498dd8a36a3
sdorado2 <sdorado2@gmail.com>
2023-02-17 18:46:48 -0500

    add array of enemies

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const createEnemyShip = () => {
   return new battleship(Math.floor(Math.random() * 4 + 3), 5, Math.random());
 };
 
-let attack = (objAtk, objDef) => {
+const attack = (objAtk, objDef) => {
   if (objAtk.accuracy > 0.5) {
     objDef.hull = objDef.hull - objAtk.firepower;
     console.log(`${objAtk.firepower} was taken by opponent`);
@@ -28,17 +28,17 @@ let attack = (objAtk, objDef) => {
   }
 };
 
-let counterAtk = (objAtk, objDef) => {
+const counterAtk = (objAtk, objDef) => {
   if (objDef.accuracy > 0.5) {
     objAtk.hull = objAtk.hull - objDef.firepower;
     console.log(`You received ${objDef.firepower}`);
     return objAtk.hull;
-  }else if (objDef.accuracy < 0.5){
-    console.log ('Counter attack was missed!');
+  } else if (objDef.accuracy < 0.5) {
+    console.log("Counter attack was missed!");
   }
 };
 
-let runAway = (obj) => {
+const runAway = (obj) => {
   console.log(
     `You have manage to escape with hull :${obj.hull} and firepower: ${obj.firepower}!\nGame Over!`
   );
@@ -47,43 +47,48 @@ let runAway = (obj) => {
 let newGame = prompt("New Game :");
 
 if (newGame == "y") {
-  let player = createPlayerShip();
-  let enemy = createEnemyShip();
   let endGame = false;
+  let player = createPlayerShip();
+  let enemy = [];
+  let counter = 0;
+
+  for (let index = 0; index < 6; index++) {
+    enemy[index] = createEnemyShip();
+  }
 
   console.log(player);
   console.log(enemy);
 
-  while (endGame !== true){
-    let ans = prompt ('Attack? : ');
+  while (endGame !== true) {
+    let ans = prompt("Attack? : ");
 
-    if (ans == 'y'){
-        if (player.hull >= 0 && enemy.hull >= 0){
-            attack (player, enemy);
-            console.log(player, enemy );
-            if (enemy.hull <= 0){ endGame = true}
+    if (ans == "y") {
+      if (player.hull >= 0 && enemy[counter].hull >= 0) {
+        attack(player, enemy[counter]);
+        console.log(player, enemy[counter]);
+        if (enemy[counter].hull <= 0 && enemy != null) {
+          enemy.shift();
+        }else{
+            console.log('Mission Complete!')
         }
-        else if (player.hull >= 0 && enemy.hull <= 0){
-            console.log("Enemy was defeated!")
-            endGame = true;
-        }
-        else if (player.hull <= 0 && enemy.hull >=0){
-            console.log ('You Lose!')
-            endGame = true;
-        }
-        else if (player.hull <= 0 && enemy.hull <= 0){
-            console.log('Love All!')
-            endGame = true;
-        }
-    }
-    else if (ans == 'n'){
-        runAway(player);
+
+
+      } else if (player.hull >= 0 && enemy[counter].hull <= 0) {
+        console.log("Enemy was defeated!");
         endGame = true;
-        
+      } else if (player.hull <= 0 && enemy[counter].hull >= 0) {
+        console.log("You Lose!");
+        endGame = true;
+      } else if (player.hull <= 0 && enemy[counter].hull <= 0) {
+        console.log("Love All!");
+        endGame = true;
+      }
+    } else if (ans == "n") {
+      runAway(player);
+      endGame = true;
     }
   }
-}
-else if (newGame == "n"){
+} else if (newGame == "n") {
   console.log("Thank You For Playing!");
 }
 
@@ -101,7 +106,6 @@ else if (newGame == "n"){
 //       endGame = true;
 //     }
 //   }
-
 
 // attack(red, blue);
 // runAway(red);
